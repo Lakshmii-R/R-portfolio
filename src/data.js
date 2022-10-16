d0b76a0b2d43ad4e25b9c900e7a1beef1c4393cd
Lakshmii-R <lakshmi.r.242001@gmail.com>
2022-10-16 22:15:40 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const projects = [
+  
+ 
+  {
+    title: "Chat",
+    subtitle: "React and Firebase",
+    description:
+      "A real time chat application that supports many users to chat simultaneously",
+    image: "./project-3.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+  {
+    title: " Todo App",
+    subtitle: "React Hooks",
+    description:
+      "A Simple app that allows users to add and delete tasks",
+    image: "./project-4.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+];
+
+
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  
+  "Node",
+  "C",
+  
+];
